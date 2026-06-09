265069d576623807c8799eb0d4c88982eb38f64e
Gaurav Chhetri <gaurav.chhetri@dealshare.in>
2026-06-09 21:53:23 +0530

    added base code

---
+++ src/main/java/com/example/butex/ButexApplication.java
@@ -0,0 +1,13 @@
+package com.example.butex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/butex/ButexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.butex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
